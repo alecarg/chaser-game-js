f55afd43a988d581eb70ddf9bca388581fba07fd
Ale <alejandrocarg@gmail.com>
2018-09-28 22:57:29 +0100

    Corrected character elevation so characters show on the right square; refactored a few other things on board.js

--- js/board.js
+++ js/board.js
@@ -50,42 +50,39 @@ board.draw = function() {
     // loop through our map and draw out the image represented by the number.
     for (var i = 0; i < board.tiles.length; i++) {
       for (var j = 0; j < board.tiles[i].length; j++) {
-        board.drawTerrain(ctx, i, j);
-        board.drawCharacters(ctx, i, j);
+        var drawX = (i - j) * board.tileH + board.mapX;
+        var drawY = (i + j) * board.tileH / 2 + board.mapY;
+        board.drawTerrain(ctx, i, j, drawX, drawY);
+        board.drawCharacters(ctx, i, j, drawX, drawY);
       }
     }
 }
 
-board.drawTerrain = function(ctx, i, j) {
-
-  var drawX = (i - j) * board.tileH + board.mapX;
-  var drawY = (i + j) * board.tileH / 2 + board.mapY;
-
-  tileToDraw = board.getTile(i, j); // 0 or 1 = map
+board.drawTerrain = function(ctx, i, j, drawX, drawY) {
+  var tileToDraw = board.getTile(i, j); // 0 or 1 = map
   ctx.drawImage(board.tileGraphics[tileToDraw], drawX, drawY);
 }
 
-board.drawCharacters = function(ctx, i, j) {
-
-  var drawX = (i - j) * board.tileH + board.mapX;
-  var drawY = (i + j) * board.tileH / 2 + board.mapY;
-  
-  var characterElevation = 52; // things "on" the board are offsetedY to look on top
+board.drawCharacters = function(ctx, i, j, drawX, drawY) {
+  var characterElevation = (board.tileH - 5); // things "on" the board are offsetedY to look on top
   var isChaserInCurrTile = (chaser.pos.x === i && chaser.pos.y === j);
   var playerInCurrTile = helpers.getPlayerByCoords(i, j);
 
   if (isChaserInCurrTile) {
+    var tileToDraw = 2;
+    ctx.drawImage(board.tileGraphics[tileToDraw], drawX, drawY - characterElevation);
+    return;
+  }
 
-    ctx.drawImage(board.tileGraphics[2], drawX, drawY - characterElevation);
-
-  } else if (playerInCurrTile) {
-
-    ctx.drawImage(board.tileGraphics[3], drawX, drawY - characterElevation);
+  if (playerInCurrTile) {
+    var tileToDraw = 3;
+    ctx.drawImage(board.tileGraphics[tileToDraw], drawX, drawY - characterElevation);
 
     // Draw player's number
     var playerUid = playerInCurrTile.number.toString();
     ctx.font = '18px Verdana';
     ctx.fillText(playerUid, drawX, drawY);
+    return;
   }
 }
 
@@ -93,7 +90,7 @@ board.getTile = function(x, y){
   try {
     var tile = board.tiles[x][y];
   } catch(e){
-    console.log('Tried to get tile outside of map.');
+    console.log('Tried to get tile outside of map. Restarting.');
     game.restart();
   }
   return tile;
