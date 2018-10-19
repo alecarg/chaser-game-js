5088c2af4cec7beb227bbaad81057332db55ff2c
Ale <alejandrocarg@gmail.com>
2018-10-19 12:36:32 +0100

    Add code comments to the player's starting code; give more room for the logger and the code

--- js/index.js
+++ js/index.js
@@ -27,5 +27,5 @@ board.offsetX = (board.canvasWidth / 2) - (board.tileW / 2);
 board.offsetY = board.tileH;
 
 var ui = {
-  startingCode: `var fiftyFifty = ((Math.random() * 100) > 50);\n\if (fiftyFifty){\n\  me.move('east');\n\} else {\n\  me.move('south');\n\}`
+  startingCode: `/*\n\*  This is your player's code.\n\*  Below there is an example of a player moving either 'east' 50% of the time or 'south' the other 50%, based on a random number generated.\n\*  We suggest you do not use that code, as you'll want something a lot more reactive to survive longer.\n\*  Think about running away from the chaser and checking you're not stepping into water or any other players!\n\*/\nvar fiftyFifty = ((Math.random() * 100) > 50);\n\if (fiftyFifty){\n\  me.move('east');\n\} else {\n\  me.move('south');\n\}`
 };
